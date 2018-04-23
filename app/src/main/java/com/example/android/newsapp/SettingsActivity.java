a5b129cb7c79e138db54dcd5c0926d6074160de3
fragargon <fragargon@gmail.com>
2018-04-23 23:30:55 +0200

    add preferences

--- app/src/main/java/com/example/android/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -18,5 +18,10 @@ public class SettingsActivity extends AppCompatActivity {
 
     public static class NewsPreferenceFragment extends PreferenceFragment {
 
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
